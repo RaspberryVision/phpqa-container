edf733b202e4ef01ce0017ced396f360a0008f4f
RafalMalik <rafalmalik.info@gmail.com>
2020-03-12 23:21:23 +0100

    Cleaned up entrypoint file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 # RUN QA TOOLS
-dephpend dot src/ -n
-#dephpend dot dms metrics uml
 
-# GREAT IDEA!
-# AFTER WHOLE QA CHECKING CONTAINER SEND LOGS (REPORTS) TO SERVICE (CHECKER?)
+# SEND RESULTS TO CHECKER API - todo
